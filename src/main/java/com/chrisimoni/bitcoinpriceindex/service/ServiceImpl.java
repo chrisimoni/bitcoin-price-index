15c41bc0631099979ffa970f257000cefa409e6b
chrisimoni <imonireal@gmail.com>
2021-10-17 16:39:57 +0100

    add unit tests and service stub

--- src/main/java/com/chrisimoni/bitcoinpriceindex/service/ServiceImpl.java
+++ src/main/java/com/chrisimoni/bitcoinpriceindex/service/ServiceImpl.java
@@ -1,5 +1,30 @@
 package com.chrisimoni.bitcoinpriceindex.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+
 public class ServiceImpl implements Service {
+
+	public Map<JSONObject, JSONObject> getCurrentBitcoinPriceIndexData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Double> getHighestAndLowestBitcoinRateInLast30Days(String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean checkCurrencyCodeExist(String code) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public double getCurrentBitcoinRate(String code) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
--- src/test/java/com/chrisimoni/bitcoinpriceindex/BitcoinPriceIndexTest.java
+++ src/test/java/com/chrisimoni/bitcoinpriceindex/BitcoinPriceIndexTest.java
@@ -1,5 +1,50 @@
 package com.chrisimoni.bitcoinpriceindex;
 
-public class BitcoinPriceIndexTest {
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import com.chrisimoni.bitcoinpriceindex.service.Service;
+
+public class BitcoinPriceIndexTest {
+	Service serviceStub = new ServiceStub();
+	BitcoinPriceIndex bitcoinPriceIndex = new BitcoinPriceIndex(serviceStub);
+	String code = "usd";
+
+	@Test
+	void testCheckCurrencyCodeExist() {
+		assertTrue(bitcoinPriceIndex.checkCurrencyCodeExist(code));
+	}
+
+	@Test
+	void testGetCurrentBitcoinPriceIndexData() {
+		Map<JSONObject, JSONObject> data = bitcoinPriceIndex.getCurrentBitcoinPriceIndexData();
+		assertEquals(1, data.size());
+	}
+
+	@Test
+	void testGetCurrentBitcoinRate() {
+		double currentBitcoinRate = bitcoinPriceIndex.getCurrentBitcoinRate(code);
+		assertEquals(60873.2183, currentBitcoinRate);
+	}
+
+	@Test
+	void testGetHighestBitcoinRateInLast30Days() {
+		List<Double> highestBitcoinRateInLast30Days = bitcoinPriceIndex
+				.getHighestAndLowestBitcoinRateInLast30Days(code);
+
+		assertEquals(61693.79, highestBitcoinRateInLast30Days.get(highestBitcoinRateInLast30Days.size() - 1));
+	}
+
+	@Test
+	void testGetLowestBitcoinRateInLast30Days() {
+		List<Double> lowestBitcoinRateInLast30Days = bitcoinPriceIndex.getHighestAndLowestBitcoinRateInLast30Days(code);
+
+		assertEquals(40681.445, lowestBitcoinRateInLast30Days.get(0));
+	}
 }
